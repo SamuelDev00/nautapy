e46298256ecc5ffec6243990bbdbd1484579e405
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-07-25 14:29:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,22 @@
-from setuptools import setup, find_packages
-from os import path
-from io import open
-
-def get_readme():
-    """
-        Get the long description from the README file
-        :return:
-        """
-    with open(path.join(here, "README.md"), encoding="utf-8") as f:
-        return f.read()
-
-
-here = path.abspath(path.dirname(__file__))
+from distutils.core import setup
 
 setup(
-    name = "nautapy",
-    version = "1.0",
-    description = "Librería sencilla para acceder al Portal Nauta",
-    long_description = get_readme(),
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/SamuelDev00/nautapy",
-    download_url = 'https://github.com/SamuelDev00/nautapy/tarball/1.0',
-    author = "SamuelDev",
-    author_email = "samuel.falconpc@gmail.com",
-    license = "GNU General Public License v3",
-    platforms=["Unix"],
-    classifiers=[
+  name = 'nautapy',        
+  packages = ['nautapy'],   
+  version = '1.0',      
+  license='GNU General Public License v3',      
+  description = 'Librería sencilla para acceder al Portal Nauta',   
+  author = 'Samuel',                
+  author_email = 'samuel.falconpc@gmail.com',      
+  url = 'https://github.com/SamuelDev00/nautapy',  
+  download_url = 'https://github.com/SamuelDev00/nautapy/archive/refs/tags/v1.0.tar.gz',    
+  keywords = ['Portal', 'Nauta', 'Etecsa'],   
+  install_requires=[
+          'requests~=2.27.1',
+          'beautifulsoup4~=4.10.0',
+          'lxml==4.7.1',
+      ],
+  classifiers=[
         "Topic :: Internet",
         "License :: OSI Approved :: GNU General Public License v3 or later "
         "(GPLv3+)",
@@ -33,7 +24,4 @@ setup(
         "Development Status :: 4 - Beta",
         "Operating System :: Unix"
     ],
-    keywords = "Portal Nauta Librería",
-    packages=find_packages(),
-    install_requires=["requests~=2.27.1", "beautifulsoup4~=4.10.0", "lxml==4.7.1"],
 )
