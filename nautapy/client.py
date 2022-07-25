d17728bc6eb0d8796b3d2c6034f234fca7d25695
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-07-25 17:18:55 -0400

    Add files via upload

--- nautapy/client.py
+++ nautapy/client.py
@@ -13,8 +13,14 @@ class PortalClient(object):
         self.captcha = ''
         self.csrf = ''
         self.cookies = ''
+        self.proxy = ''
+        if self.proxy == '':
+             pass
+        else:
+             self.proxy = dict(https=self.proxy)
         self.headers = {'content-type': 'application/x-www-form-urlencoded', "User-agent":"Mozilla/5.0 (X11; Linux x86_64)"}
-    
+
+
     #diccionario para obtener los datos de la cuenta
     datos = {"username": "usuario",
              "account_type": "tipo de cuenta",
@@ -24,20 +30,20 @@ class PortalClient(object):
              "mail_account": "cuenta de correo"}
 
     def get_csrf(self, url):
-        resp = self.session.get(url, headers=self.headers)
+        resp = self.session.get(url, proxies=self.proxy, headers=self.headers)
         soup = BeautifulSoup(resp.text,'html.parser')
         csrf = soup.find('input',attrs={'name':'csrf'})['value']
         return csrf
 
     def get_captcha(self):
         url_captcha = self.url+'captcha/?'
-        captcha = self.session.get(url_captcha, headers=self.headers).content
+        captcha = self.session.get(url_captcha, proxies=self.proxy, headers=self.headers).content
         return captcha
         
     def login(self, captcha):
         try: 
              login = self.url+'user/login/es-es'
-             resp = self.session.get(login, headers=self.headers)
+             resp = self.session.get(login, proxies=self.proxy, headers=self.headers)
              user = self.username
              contra = self.password
              self.cookies = resp.cookies
@@ -48,14 +54,14 @@ class PortalClient(object):
                         'password_user': contra, 
                         'captcha': self.captcha,
                         'btn_submit': ''}
-             resp2 = self.session.post(login, data=payload, headers=self.headers, cookies=self.cookies)
+             resp2 = self.session.post(login, data=payload, proxies=self.proxy, headers=self.headers, cookies=self.cookies)
              if resp2.text.count('"msg_error">'):
                   mensaje_error = resp2.text.split('"msg_error">')[1].split("<")[0]
                   return print(f'No pude iniciar sesión: {mensaje_error}')
              else:
                   return print('\nHe iniciado sesión\n')
         except Exception as ex:
-            print(ex)
+            return print(ex)
     
 
     def get_info(self,attr):
@@ -65,7 +71,7 @@ class PortalClient(object):
             index = 0
         count = 0
         url = self.url+'useraaa/user_info'
-        resp = self.session.get(url, headers=self.headers)
+        resp = self.session.get(url, proxies=self.proxy, headers=self.headers)
         soup = BeautifulSoup(resp.text,'html.parser')
         for div in soup.find_all("div", {"class": "col s12 m6"}):
             if div.find("h5").text.strip().lower() == PortalClient.datos[attr]:
@@ -81,7 +87,7 @@ class PortalClient(object):
         payload = {'csrf': self.csrf,
                    'recharge_code': code,
                    'btn_submit': ''}
-        resp = self.session.post(url, data=payload, headers=self.headers)
+        resp = self.session.post(url, data=payload, proxies=self.proxy, headers=self.headers)
         error = resp.text
         if error.count("msg_error"):
             mensaje_error = error.split('"msg_error">')[1].split("<")[0]
@@ -101,7 +107,7 @@ class PortalClient(object):
                    'password_user': password, 
                    'id_cuenta': account_transfer,
                    'action': 'checkdata'}
-        resp = self.session.post(url, data=payload, headers=self.headers)
+        resp = self.session.post(url, data=payload, proxies=self.proxy, headers=self.headers)
         error = resp.text
         root_element5 = lxml.html.fromstring(error)
         saldo = root_element5.xpath('//div[@class="card-panel"]/div/div/p')[0] 
@@ -124,7 +130,7 @@ class PortalClient(object):
                    'new_password': new_password, 
                    'repeat_new_password': new_password,
                    'btn_submit': ''}
-        resp = self.session.post(url, data=payload, headers=self.headers)
+        resp = self.session.post(url, data=payload, proxies=self.proxy, headers=self.headers)
         error = resp.text
         if error.count("msg_error"):
             mensaje_error = error.split('"msg_error">')[1].split("<")[0]
@@ -144,7 +150,7 @@ class PortalClient(object):
                    'new_password': new_password, 
                    'repeat_new_password': new_password,
                    'btn_submit': ''}
-        resp = self.session.post(url, data=payload, headers=self.headers)
+        resp = self.session.post(url, data=payload, proxies=self.proxy, headers=self.headers)
         error = resp.text
         if error.count("msg_error"):
             mensaje_error = error.split('"msg_error">')[1].split("<")[0]
@@ -154,6 +160,3 @@ class PortalClient(object):
              return print(mensaje_error2)
         else:
              return print('Error Desconocido')
-
-
-
